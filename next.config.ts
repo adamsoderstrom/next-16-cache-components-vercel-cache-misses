45ada16880d75504b9f7cc1ae3c0ae605eba1f90
adamsoderstrom <adamsoderstrom@me.com>
2025-12-11 23:44:57 +0100

    try out `16.0.0-canary.0`
    To see if the error could be recreated in that version as well.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  cacheComponents: true
+  experimental: {
+    cacheComponents: true
+  }
 };
 
 export default nextConfig;
